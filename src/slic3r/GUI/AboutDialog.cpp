4547777663fbfa71177b75b5cbc1ec277a390bf2
SoftFever <softfeverever@gmail.com>
2024-05-21 00:21:35 +0800

    update locale

--- src/slic3r/GUI/AboutDialog.cpp
+++ src/slic3r/GUI/AboutDialog.cpp
@@ -244,7 +244,7 @@ AboutDialog::AboutDialog()
         vesizer->Add(0, FromDIP(185), 1, wxEXPAND, FromDIP(5));
          auto          version_string = _L("V") + " " + std::string(Orca_Flashforge_VERSION);
         wxStaticText* version = new wxStaticText(this, wxID_ANY, version_string.c_str(), wxDefaultPosition, wxDefaultSize);
-        //wxStaticText* bs_version = new wxStaticText(this, wxID_ANY, wxString::Format("Based on BambuStudio and PrusaSlicer"), wxDefaultPosition, wxDefaultSize);
+        //wxStaticText* bs_version = new wxStaticText(this, wxID_ANY, wxString::Format("Based on PrusaSlicer and BambuStudio"), wxDefaultPosition, wxDefaultSize);
         //bs_version->SetFont(Label::Body_12);
         wxFont version_font = GetFont();
         #ifdef __WXMSW__
