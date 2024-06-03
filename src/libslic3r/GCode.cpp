bd7e85227e65aeec29330660d5a8e2cf386dc1c1
SoftFever <softfeverever@gmail.com>
2024-06-03 21:30:38 +0800

    Thumbnails feature revamp. (#5555)
    * Thumbnails feature revamp.
    Support generating different size/format combinations
    * misc fix
    
    Co-authored-by: Lukas Matena <lukasmatena@seznam.cz>

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -2067,15 +2067,18 @@ void GCode::_do_export(Print& print, GCodeOutputStream &file, ThumbnailsGenerato
                 print.config().nozzle_temperature_initial_layer.get_at(0));
             file.write("; CONFIG_BLOCK_END\n\n");
         } else if (thumbnail_cb != nullptr) {
-            if (m_gcode_thumbnail_format != GCodeThumbnailsFormat::BTT_TFT) {
-                auto thumbnaim_fmt = m_gcode_thumbnail_format;
-                // Orca: if the thumbnail format is ColPic, we write PNG in the beginning of gcode file and ColPic in the end of gcode file. 
-                if (m_gcode_thumbnail_format == GCodeThumbnailsFormat::ColPic)
-                    thumbnaim_fmt = GCodeThumbnailsFormat::PNG;
-                GCodeThumbnails::export_thumbnails_to_file(
-                    thumbnail_cb, print.get_plate_index(), print.full_print_config().option<ConfigOptionPoints>("thumbnails")->values,
-                    thumbnaim_fmt, [&file](const char* sz) { file.write(sz); }, [&print]() { print.throw_if_canceled(); });
+            // generate the thumbnails
+            auto [thumbnails, errors] = GCodeThumbnails::make_and_check_thumbnail_list(print.full_print_config());
+
+            if (errors != enum_bitmask<ThumbnailError>()) {
+                std::string error_str = format("Invalid thumbnails value:");
+                error_str += GCodeThumbnails::get_error_string(errors);
+                throw Slic3r::ExportError(error_str);
             }
+
+            if (!thumbnails.empty())
+                GCodeThumbnails::export_thumbnails_to_file(
+                    thumbnail_cb, print.get_plate_index(), thumbnails, [&file](const char* sz) { file.write(sz); }, [&print]() { print.throw_if_canceled(); });
         }
     }
 
@@ -2789,11 +2792,6 @@ void GCode::_do_export(Print& print, GCodeOutputStream &file, ThumbnailsGenerato
                 GCodeProcessor::ETags::Estimated_Printing_Time_Placeholder)
             .c_str());
       file.write("\n");
-      if (m_gcode_thumbnail_format == GCodeThumbnailsFormat::ColPic)
-            GCodeThumbnails::export_thumbnails_to_file(
-                thumbnail_cb, print.get_plate_index(), print.full_print_config().option<ConfigOptionPoints>("thumbnails")->values,
-                m_gcode_thumbnail_format, [&file](const char* sz) { file.write(sz); }, [&print]() { print.throw_if_canceled(); });
-
       file.write("; CONFIG_BLOCK_START\n");
       std::string full_config;
       append_full_config(print, full_config);
