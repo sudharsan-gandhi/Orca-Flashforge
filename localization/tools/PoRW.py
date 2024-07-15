4214d75a662ef89c9fa49d0b3c7b1748331692a3
linnaiyuan <lny@flashforge.com>
2024-07-15 13:56:29 +0800

    merge multiple lines of msgid

--- localization/tools/PoRW.py
+++ localization/tools/PoRW.py
@@ -1,65 +1,77 @@
 class Msg(object):
     def __init__(self):
-        self.msgCtxt = ""
-        self.msgId = ""
-        self.msgIdPlural = ""
-        self.msgStr = ""
-        self.msgStrPlural = ""
+        self.msgCtxt = None
+        self.msgId = None
+        self.msgIdPlural = None
+        self.msgStr = None
+        self.msgStrPlural = None
         self.lines = []
 
-def procLineWithKey(lineVal, msg, attr):
+def _procLineWithKey(lineVal, msg, attr):
     val = getattr(msg, attr)
-    if len(val) != 0:
+    if val != None:
         return False
     if lineVal[0] != '\"' or lineVal[-1] != '\"':
         return False
-    setattr(msg, attr, val + lineVal)
+    setattr(msg, attr, lineVal[1:-1])
     return True
 
-def parseMsg(msg):
-    attr = ""
+def _parseMsg(msg):
+    attr = None
     for line in msg.lines:
         lineTrimed = line.strip()
         if lineTrimed.startswith("msgctxt") and lineTrimed[7].isspace():
-            if procLineWithKey(lineTrimed[7:].strip(), msg, "msgCtxt"):
+            if _procLineWithKey(lineTrimed[7:].strip(), msg, "msgCtxt"):
                 attr = "msgCtxt"
             else:
                 return False
         elif lineTrimed.startswith("msgid") and lineTrimed[5].isspace():
-            if procLineWithKey(lineTrimed[5:].strip(), msg, "msgId"):
+            if _procLineWithKey(lineTrimed[5:].strip(), msg, "msgId"):
                 attr = "msgId"
             else:
                 return False
         elif lineTrimed.startswith("msgid_plural") and lineTrimed[12].isspace():
-            if procLineWithKey(lineTrimed[12:].strip(), msg, "msgIdPlural"):
+            if _procLineWithKey(lineTrimed[12:].strip(), msg, "msgIdPlural"):
                 attr = "msgIdPlural"
             else:
                 return False
         elif lineTrimed.startswith("msgstr") and lineTrimed[6].isspace():
-            if procLineWithKey(lineTrimed[6:].strip(), msg, "msgStr"):
+            if _procLineWithKey(lineTrimed[6:].strip(), msg, "msgStr"):
                 attr = "msgStr"
             else:
                 return False
         elif lineTrimed.startswith("msgstr[0]") and lineTrimed[9].isspace():
-            if procLineWithKey(lineTrimed[9:].strip(), msg, "msgStr"):
+            if _procLineWithKey(lineTrimed[9:].strip(), msg, "msgStr"):
                 attr = "msgStr"
             else:
                 return False
         elif lineTrimed.startswith("msgstr[1]") and lineTrimed[9].isspace():
-            if procLineWithKey(lineTrimed[9:].strip(), msg, "msgStrPlural"):
+            if _procLineWithKey(lineTrimed[9:].strip(), msg, "msgStrPlural"):
                 attr = "msgStrPlural"
             else:
                 return False
-        elif lineTrimed[0] == '\"' and lineTrimed[-1] == '\"':
-            if len(attr) == 0:
+        elif lineTrimed[0] == "\"" and lineTrimed[-1] == "\"":
+            if attr == None:
                 return False
             else:
-                setattr(msg, attr, getattr(msg, attr) + lineTrimed)
+                setattr(msg, attr, getattr(msg, attr) + lineTrimed[1:-1])
         elif lineTrimed.startswith("#"):
-            attr = ""
+            attr = None
         else:
             return False
     return True
+
+def _printBadMsg(msg, pos, fileName):
+    if all(line.strip().startswith("#") for line in msg.lines):
+        return
+    if msg.msgId == "" and msg.msgStr != None:
+        for line in msg.lines:
+            if line.lstrip().startswith("\"Project-Id-Version:"):
+                return
+    print("invalid message %d, %s" % (pos, fileName))
+    for line in msg.lines:
+        print(line, end='')
+    print()
 
 def readMsgList(fileName):
     msg = Msg()
@@ -68,15 +80,10 @@ def readMsgList(fileName):
     for line in open(fileName, encoding='utf-8').readlines():
         if len(line.strip()) == 0:
             if len(msg.lines) != 0:
-                if not parseMsg(msg):
-                    print("invalid message %d, %s" % (len(msgList), fileName))
-                    for line in msg.lines:
-                        print(line, end='')
-                    print()
-                    InvalidMsgList.append(msg)
-                elif len(msg.msgId) != 0 and len(msg.msgStr) != 0:
+                if _parseMsg(msg) and msg.msgId != None and len(msg.msgId) > 0 and msg.msgStr != None:
                     msgList.append(msg)
                 else:
+                    _printBadMsg(msg, len(msgList), fileName)
                     InvalidMsgList.append(msg)
             msg = Msg()
         else:
