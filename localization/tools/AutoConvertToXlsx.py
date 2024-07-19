1a31739d96ab0c28c5b3d257faa33c4a567551a0
linnaiyuan <lny@flashforge.com>
2024-07-19 14:35:04 +0800

    rename

--- localization/tools/AutoConvertToXlsx.py
+++ localization/tools/AutoConvertToXlsx.py
@@ -5,7 +5,7 @@ from convertPoToXlsx import convertPoToXlsx
 if __name__ == '__main__':
     try:
         app_dir = os.path.dirname(os.path.abspath(__file__))
-        for lan in ["de", "en", "es", "fr", "ja", "ko"]:
+        for lan in ["de", "en", "es", "fr", "ja", "ko", "lt"]:
             poFileName = "flashforge_%s.po" % lan
             poFilePath = os.path.join(app_dir, "../flashforge", lan, poFileName)
             xlsxFilePath = os.path.join(app_dir, "flashforge_%s.xlsx" % lan)
