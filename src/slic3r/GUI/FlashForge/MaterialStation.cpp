cb10d14d96e2c8f7f22a531f592daf7e3092a93d
huyuanhang <yuanhang.hu@flashforge.com>
2024-09-02 19:33:59 +0800

    去掉self=null

--- src/slic3r/GUI/FlashForge/MaterialStation.cpp
+++ src/slic3r/GUI/FlashForge/MaterialStation.cpp
@@ -968,7 +968,7 @@ MaterialSlotArea::MaterialSlotArea(wxWindow* parent, wxWindowID id, const wxPoin
     s_self = this;
 }
 
-MaterialSlotArea::~MaterialSlotArea() { s_self = nullptr; }
+MaterialSlotArea::~MaterialSlotArea() { }
 
 MaterialSlotArea* MaterialSlotArea::get_inst() { return s_self; }
 
