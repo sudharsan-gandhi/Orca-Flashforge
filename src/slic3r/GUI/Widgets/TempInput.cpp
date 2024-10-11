9edd0ea6ff2913e5863a87589410f72ff549cefd
yuanminda <minda.yuan@flashforge.com>
2024-10-11 17:19:36 +0800

    modify air filter  height；

--- src/slic3r/GUI/Widgets/TempInput.cpp
+++ src/slic3r/GUI/Widgets/TempInput.cpp
@@ -1078,7 +1078,7 @@ void StartFiltering::create_panel(wxWindow* parent)
     sizer->AddSpacer(FromDIP(14));
     sizer->Add(external_circulate_panel, 0, wxEXPAND | wxALIGN_CENTER, 0);
 #ifdef __WIN32__
-    sizer->AddSpacer(FromDIP(140));
+    sizer->AddSpacer(FromDIP(173));
 #else if __APPLE__
     sizer->AddSpacer(FromDIP(151));
 #endif
