9fac10441663a9c726b460091fe97b7fdc0b756d
linnaiyuan <lny@flashforge.com>
2025-05-22 14:44:10 +0800

    删除不用的变量

--- src/slic3r/GUI/FlashForge/MultiComMgr.hpp
+++ src/slic3r/GUI/FlashForge/MultiComMgr.hpp
@@ -149,7 +149,6 @@ private:
     std::unique_ptr<ComThreadPool>           m_threadPool;
     WaitEvent                                m_threadExitEvent;
     interprocess_file_lock_ptr_t             m_nimDataDirFileLock;
-    bool                                     m_blockShowNimDataBaseError;
     std_precise_clock::time_point            m_showNimDataBaseErrorTime;
     const char                              *m_nimDataFileLockName;
 };
