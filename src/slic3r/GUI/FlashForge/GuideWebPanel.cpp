015c7ef71bc0c07158e9c780e1d4456a81645b3d
gengtangrui <tangrui.gen@flashforge.com>
2025-07-04 09:59:01 +0800

    修复指引界面布局不能自适应的问题

--- src/slic3r/GUI/FlashForge/GuideWebPanel.cpp
+++ src/slic3r/GUI/FlashForge/GuideWebPanel.cpp
@@ -1,4 +1,5 @@
 #include "GuideWebPanel.h"
+#include "slic3r/GUI/Widgets/Label.hpp"
 #include <wx/graphics.h>
 
 #define LOADING_INTERVAL 200
@@ -10,7 +11,7 @@ wxDEFINE_EVENT(EVT_LOADING_TIMEOUT, wxCommandEvent);
 GuideWebPanel::GuideWebPanel(wxWindow* parent, wxWindowID id) : 
 	wxPanel(parent, id, wxDefaultPosition, wxDefaultSize), m_url("https://www.flashforge.com/")
 { 
-    
+    SetDoubleBuffered(true);
 	auto* sizer = new wxBoxSizer(wxVERTICAL);
     m_web_view        = WebView::CreateWebView(this, m_url);
     m_web_view->SetMinSize(GetClientSize());
@@ -31,16 +32,14 @@ GuideWebPanel::GuideWebPanel(wxWindow* parent, wxWindowID id) :
     m_error_panel->SetMinSize(GetClientSize());
     wxPanel* error_content = new wxPanel(m_error_panel, wxID_ANY);
     wxBoxSizer* error_sizer   = new wxBoxSizer(wxVERTICAL);
-    ScalableButton* error_icon    = new ScalableButton(m_error_panel, wxID_ANY, "web_error", "", FromDIP(wxSize(60, 60)), wxDefaultPosition, 2097153L, false, 60);
+    ScalableButton* error_icon    = new ScalableButton(m_error_panel, wxID_ANY, "web_error", "", FromDIP(wxSize(48, 48)), wxDefaultPosition, 2097153L, false, 48);
     auto            error_text    = new wxStaticText(m_error_panel, wxID_ANY, _L("Load failed, try again"));
     auto            again_btn     = new FFButton(m_error_panel, wxID_ANY, _L("Try Again"), 8);
     error_icon->SetBackgroundColour(*wxWHITE);
-    auto font = error_text->GetFont();
-    font.SetPointSize(FromDIP(20));
-    error_text->SetFont(font);
-    again_btn->SetFont(font);
-    again_btn->SetMinSize(FromDIP(wxSize(160, 60)));
-    again_btn->SetSize(FromDIP(wxSize(160, 60)));
+    error_text->SetFont(Label::Body_16);
+    again_btn->SetFont(Label::Body_16);
+    again_btn->SetMinSize(FromDIP(wxSize(128, 48)));
+    again_btn->SetSize(FromDIP(wxSize(128, 48)));
     again_btn->SetBorderColor(wxColor(65, 148, 136));
     again_btn->SetFontColor(wxColor(65, 148, 136));
     again_btn->Bind(wxEVT_BUTTON, [&](wxCommandEvent& evnet) { 
@@ -123,15 +122,15 @@ void LoadingWebPage::OnPaint(wxPaintEvent& event)
     gc->SetBrush(wxColor(*wxWHITE));
     gc->DrawRectangle(0, 0, GetClientSize().x, GetClientSize().y);
     gc->SetBrush(*wxTRANSPARENT_BRUSH);
-    int       radius     = 40;
-    int       rangeAngle = 120;
     wxPoint   center(GetClientSize().x / 2, GetClientSize().y / 2);
     wxBitmap& bmp = m_loadingIcons[m_loadingIdx].bmp();
-    gc->DrawBitmap(bmp, center.x - bmp.GetWidth() / 2, center.y - bmp.GetHeight(), bmp.GetWidth(), bmp.GetHeight());
-    wxFont font;
-    font.SetPointSize(FromDIP(20));
-    gc->SetFont(font, *wxBLACK);
-    gc->DrawText(_L("Loading..."), center.x - FromDIP(50), center.y + FromDIP(15));
+    gc->DrawBitmap(bmp, center.x - bmp.GetWidth() / 2, center.y - bmp.GetHeight() / 2 - FromDIP(10), bmp.GetWidth(), bmp.GetHeight());
+    auto font = Label::Body_16;
+    gc->SetFont(font, wxColour(51, 51, 51));
+    
+    auto str      = wxString(_L("Loading..."));
+    auto textSize = dc.GetMultiLineTextExtent(str);
+    gc->DrawText(str, center.x - textSize.x / 2, center.y - textSize.y + FromDIP(60));
     delete gc;
 }
 
