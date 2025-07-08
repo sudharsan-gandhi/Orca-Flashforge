529d24cbda0bfd1989477a56f3fd1d5d0ccda3fa
linnaiyuan <lny@flashforge.com>
2025-07-08 11:14:40 +0800

    添加简单的无标题栏窗口基类

---
+++ src/slic3r/GUI/FlashForge/FFTitleLessDialog.cpp
@@ -0,0 +1,82 @@
+#include "FFTitleLessDialog.hpp"
+#include <memory>
+#include <wx/dcclient.h>
+#include <wx/graphics.h>
+
+namespace Slic3r { namespace GUI {
+
+FFTitleLessDialog::FFTitleLessDialog(wxWindow *parent)
+    : wxDialog(parent, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxFRAME_SHAPED | wxNO_BORDER)
+    , m_radius(FromDIP(6))
+    , m_isPressClose(false)
+    , m_closeBmp(this, "title_less_close", 12)
+{
+    SetBackgroundColour(*wxWHITE);
+    Bind(wxEVT_PAINT, &FFTitleLessDialog::onPaint, this);
+    Bind(wxEVT_SIZE, &FFTitleLessDialog::onSize, this);
+    Bind(wxEVT_LEFT_DOWN, &FFTitleLessDialog::onLeftDown, this);
+    Bind(wxEVT_LEFT_UP, &FFTitleLessDialog::onLeftUp, this);
+    Bind(wxEVT_MOUSE_CAPTURE_LOST, &FFTitleLessDialog::onMouseCaptureLost, this);
+}
+
+void FFTitleLessDialog::onPaint(wxPaintEvent &event)
+{
+    wxPaintDC dc(this);
+    std::unique_ptr<wxGraphicsContext> gc(wxGraphicsContext::Create(dc));
+    if (gc == nullptr) {
+        return;
+    }
+    gc->SetPen(wxColour("#c1c1c1"));
+    gc->SetBrush(*wxTRANSPARENT_BRUSH);
+    gc->DrawRoundedRectangle(0, 0, GetSize().x - 1, GetSize().y - 1, m_radius);
+    drawBackground(gc.get());
+    gc->DrawBitmap(m_closeBmp.bmp(), m_closeRect.x, m_closeRect.y, m_closeRect.width, m_closeRect.height);
+}
+
+void FFTitleLessDialog::onSize(wxSizeEvent &event)
+{
+    wxEventBlocker blocker(this, wxEVT_SIZE);
+    wxGraphicsPath path = wxGraphicsRenderer::GetDefaultRenderer()->CreatePath();
+    path.AddRoundedRectangle(0, 0, GetSize().x, GetSize().y, m_radius);
+    SetShape(path);
+
+    int margin = FromDIP(10);
+    m_closeRect.x = GetSize().x - margin - m_closeBmp.GetBmpSize().x;
+    m_closeRect.y = margin;
+    m_closeRect.width = m_closeBmp.GetBmpSize().x;
+    m_closeRect.height = m_closeBmp.GetBmpSize().y;
+}
+
+void FFTitleLessDialog::onLeftDown(wxMouseEvent &event)
+{
+    if (!m_closeRect.Contains(event.GetPosition())) {
+        event.Skip();
+        return;
+    }
+    m_isPressClose = true;
+    if (!HasCapture()) {
+        CaptureMouse();
+    }
+}
+
+void FFTitleLessDialog::onLeftUp(wxMouseEvent &event)
+{
+    if (!m_isPressClose) {
+        event.Skip();
+        return;
+    }
+    if (m_closeRect.Contains(event.GetPosition())) {
+        EndModal(wxID_CANCEL);
+    }
+    m_isPressClose = false;
+    if (HasCapture()) {
+        ReleaseMouse();
+    }
+}
+
+void FFTitleLessDialog::onMouseCaptureLost(wxMouseCaptureLostEvent &event)
+{
+    m_isPressClose = false;
+}
+
+}} // Slic3r::GUI
---
+++ src/slic3r/GUI/FlashForge/FFTitleLessDialog.hpp
@@ -0,0 +1,38 @@
+#ifndef slic3r_GUI_FFTitleLessDialog_hpp_
+#define slic3r_GUI_FFTitleLessDialog_hpp_
+
+#include <wx/dialog.h>
+#include <wx/event.h>
+#include <wx/gdicmn.h>
+#include "slic3r/GUI/wxExtensions.hpp"
+
+namespace Slic3r { namespace GUI {
+
+class FFTitleLessDialog : public wxDialog
+{
+public:
+    FFTitleLessDialog(wxWindow *parent);
+
+    virtual void drawBackground(wxGraphicsContext *gc) {}
+
+private:
+    void onPaint(wxPaintEvent &event);
+
+    void onSize(wxSizeEvent &event);
+
+    void onLeftDown(wxMouseEvent &event);
+
+    void onLeftUp(wxMouseEvent &event);
+
+    void onMouseCaptureLost(wxMouseCaptureLostEvent &event);
+
+private:
+    int    m_radius;
+    bool   m_isPressClose;
+    wxRect m_closeRect;
+    ScalableBitmap m_closeBmp;
+};
+
+}} // Slic3r::GUI
+
+#endif
