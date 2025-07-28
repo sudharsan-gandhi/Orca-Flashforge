e0b83fda7a2d20f730ef6a08f2484997777b7ef1
linnaiyuan <lny@flashforge.com>
2025-07-28 16:12:53 +0800

    AI生成模型添加积分不足错误码

--- src/slic3r/GUI/FlashForge/MultiComUtils.cpp
+++ src/slic3r/GUI/FlashForge/MultiComUtils.cpp
@@ -230,6 +230,8 @@ ComErrno MultiComUtils::fnetRet2ComErrno(int networkRet)
         return COM_DEVICE_HAS_BEEN_BOUND;
     case FNET_ABORT_AI_MODEL_JOB_FAILED:
         return COM_ABORT_AI_MODEL_JOB_FAILED;
+    case COM_AI_MODEL_JOB_NOT_ENOUGH_POINTS:
+        return COM_AI_MODEL_JOB_NOT_ENOUGH_POINTS;
     case FNET_NIM_SEND_ERROR:
         return COM_NIM_SEND_ERROR;
     case FNET_NIM_DATA_BASE_ERROR:
