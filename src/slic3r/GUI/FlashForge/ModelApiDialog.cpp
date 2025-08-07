b4d536330f14ee34d17de029c2e49abbb3f21e24
gengtangrui <tangrui.gen@flashforge.com>
2025-08-07 10:17:52 +0800

    暂时切换为tripo

--- src/slic3r/GUI/FlashForge/ModelApiDialog.cpp
+++ src/slic3r/GUI/FlashForge/ModelApiDialog.cpp
@@ -844,7 +844,7 @@ void ModelGenerateDialog::SetImgPath(wxString path)
         com_ai_model_job_result_t result;
         //result.jobId = 0;
         //result.isOldJob = false;
-        ret = MultiComHelper::inst()->startAiModelJob(1, img_url, generateFormat, result, msTimeout);
+        ret = MultiComHelper::inst()->startAiModelJob(2, img_url, generateFormat, result, msTimeout);
         if (ret != COM_OK) {
             task->safeFunc([task, ret]() {
                 auto event = new wxCommandEvent(EVT_ERROR_MSG);
