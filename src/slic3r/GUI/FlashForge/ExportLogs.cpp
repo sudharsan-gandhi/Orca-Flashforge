88bdbc33bde73874a92cfa00cfd128e220e17b3e
linnaiyuan <lny@flashforge.com>
2025-08-21 19:57:39 +0800

    导出日志后消息框弹出添加CallAfter

--- src/slic3r/GUI/FlashForge/ExportLogs.cpp
+++ src/slic3r/GUI/FlashForge/ExportLogs.cpp
@@ -45,15 +45,19 @@ void ExportLogsDlg::onExportLogsFinished(ExportLogsFinishedEvent &event)
 {
     EndModal(wxID_OK);
     if (event.succeed) {
-        MessageDialog dlg(wxGetApp().mainframe, _CTX("Export successful", "Flashforge"), _L("Export log"));
-        dlg.ShowModal();
-        wxLaunchDefaultApplication(wxFileName(event.outputPath).GetPath());
+        CallAfter([outputPath = event.outputPath]() {
+            MessageDialog dlg(wxGetApp().mainframe, _CTX("Export successful", "Flashforge"), _L("Export log"));
+            dlg.ShowModal();
+            wxLaunchDefaultApplication(wxFileName(outputPath).GetPath());
+        });
     } else {
-        MessageDialog dlg(wxGetApp().mainframe, _L("Export failed, please try again"), _L("Export log"));
-        dlg.ShowModal();
-        if (wxFileName::FileExists(event.outputPath)) {
-            wxRemoveFile(event.outputPath);
-        }
+        CallAfter([outputPath = event.outputPath]() {
+            MessageDialog dlg(wxGetApp().mainframe, _L("Export failed, please try again"), _L("Export log"));
+            dlg.ShowModal();
+            if (wxFileName::FileExists(outputPath)) {
+                wxRemoveFile(outputPath);
+            }
+        });
     }
 }
 
