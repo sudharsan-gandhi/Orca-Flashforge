35691231c220e1e6f079977ea2d6a6ebd4f33856
linnaiyuan <lny@flashforge.com>
2025-11-06 17:11:24 +0800

    添加E0113~E0123固件错误码

--- src/slic3r/GUI/FlashForge/PrinterErrorMsgDlg.cpp
+++ src/slic3r/GUI/FlashForge/PrinterErrorMsgDlg.cpp
@@ -356,6 +356,50 @@ void PrinterErrorMsgDlg::initErrorCodeDataMap()
     pair = s_errorCodeDataMap.emplace("E0112", error_code_data_t());
     pair.first->second.message = "Leveling triggered early!";
     pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0112-leveling-triggered-early";
+
+    pair = s_errorCodeDataMap.emplace("E0113", error_code_data_t());
+    pair.first->second.message = "Extruder filament sensor error";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0113-filament-is-detected-in-the-extruder-please-check-the-feeding-position-and-clean-it-manually-or-check-the-condition-of-the-nozzle-sensor";
+
+    pair = s_errorCodeDataMap.emplace("E0114", error_code_data_t());
+    pair.first->second.message = "IFS homing error";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0114-ifs-homing-error";
+
+    pair = s_errorCodeDataMap.emplace("E0115", error_code_data_t());
+    pair.first->second.message = "Lidar focus failure detected. Please check the Lidar.";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0115-the-laser-radar-has-failed-to-focus-please-check-the-laser-radar";
+
+    pair = s_errorCodeDataMap.emplace("E0116", error_code_data_t());
+    pair.first->second.message = "Exception in priming_handler, please copy the logs!";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0116-exception-in-priming_handler-please-copy-the-logs";
+
+    pair = s_errorCodeDataMap.emplace("E0117", error_code_data_t());
+    pair.first->second.message = "Exception in flush_handler, please copy the logs!";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0117-exception-in-flush_handler-please-copy-the-logs";
+
+    pair = s_errorCodeDataMap.emplace("E0118", error_code_data_t());
+    pair.first->second.message = "Internal error on command, please copy the logs!";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0118-internal-error-on-command-please-copy-the-logs";
+
+    pair = s_errorCodeDataMap.emplace("E0119", error_code_data_t());
+    pair.first->second.message = "Timing error, please copy the logs!";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0119-timing-error-please-copy-the-logs";
+
+    pair = s_errorCodeDataMap.emplace("E0120", error_code_data_t());
+    pair.first->second.message = "System restarted, please copy the logs!";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0120-system-restarted-please-copy-the-logs";
+
+    pair = s_errorCodeDataMap.emplace("E0121", error_code_data_t());
+    pair.first->second.message = "The \"spi_transfer_response\" cannot be obtained.!";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0121-the-spi_transfer_response-cannot-be-obtained";
+
+    pair = s_errorCodeDataMap.emplace("E0122", error_code_data_t());
+    pair.first->second.message = "Extruder temperature error!";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0122-extruder-temperature-error";
+
+    pair = s_errorCodeDataMap.emplace("E0123", error_code_data_t());
+    pair.first->second.message = "platform temperature error!";
+    pair.first->second.wikiUrl = "https://wiki.flashforge.com/en/ad5x/error_code_list_ad5x#e0123-platform-temperature-error";
 }
 
 }} // namespace Slic3r::GUI
