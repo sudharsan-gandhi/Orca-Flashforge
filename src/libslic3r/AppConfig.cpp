3e26d4f3e9122e8dae775b799f8a3b5c8e9c51ec
linnaiyuan <lny@flashforge.com>
2025-12-26 15:31:53 +0800

    删除错误配置的地址避免退出时出现等待

--- src/libslic3r/AppConfig.cpp
+++ src/libslic3r/AppConfig.cpp
@@ -39,8 +39,8 @@ using namespace nlohmann;
 
 namespace Slic3r {
 
-static const std::string VERSION_CHECK_URL = "http://update.cn.sz3dp.com:20080/3dapp/public/Orca-Flashforge/appInfo.json";
-static const std::string PROFILE_UPDATE_URL = "http://update.cn.sz3dp.com:20080/3dapp/public/Orca-Flashforge/appInfo.json";
+static const std::string VERSION_CHECK_URL = "";
+static const std::string PROFILE_UPDATE_URL = "";
 static const std::string MODELS_STR = "models";
 
 const std::string AppConfig::SECTION_FILAMENTS = "filaments";
