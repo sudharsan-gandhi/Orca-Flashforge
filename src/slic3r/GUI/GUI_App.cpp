b0a5c477e84de955cc59d625210ced1395953a77
linnaiyuan <lny@flashforge.com>
2026-01-07 17:36:10 +0800

    修改语言后默认tab页设置为home

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -3640,7 +3640,7 @@ void GUI_App::recreate_GUI(const wxString &msg_name)
     mainframe = new MainFrame();
     if (is_editor())
         // hide settings tabs after first Layout
-        mainframe->select_tab(size_t(MainFrame::tp3DEditor));
+        mainframe->select_tab(size_t(MainFrame::tpHome));
     // Propagate model objects to object list.
     sidebar().obj_list()->init();
     //sidebar().aux_list()->init_auxiliary();
