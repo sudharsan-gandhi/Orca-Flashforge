2ad77359004c83cf373bebdde1fe2d95a5755bf4
gengtangrui <tangrui.gen@flashforge.com>
2026-01-09 16:46:49 +0800

    更改更新app前往查看的网址

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -4597,7 +4597,7 @@ void GUI_App::handle_login_result(const std::string &token, const com_add_wan_de
                                     _L("New Product"), wxOK | wxCANCEL);
                             dlg.SetButtonLabel(wxID_OK, _L("Learn More"));
                             if (dlg.ShowModal() == wxID_OK) {
-                                wxLaunchDefaultBrowser("https://desktop.voxelshare.com/privacy/download_notice.html", wxBROWSER_NEW_WINDOW);
+                                wxLaunchDefaultBrowser("https://desktop.voxelshare.com/privacy/desktop_notice.html", wxBROWSER_NEW_WINDOW);
                             }
                         });
                     }
