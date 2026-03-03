00d6d7784dfa57e0b01b6d0c216d94a1cb4847e9
gengtangrui <tangrui.gen@flashforge.com>
2026-03-03 16:40:04 +0800

    同步meshy对打开flash studio要求的修改

--- src/libslic3r/Utils.hpp
+++ src/libslic3r/Utils.hpp
@@ -218,11 +218,13 @@ extern bool is_json_file(const std::string& path);
 
 // Orca: custom protocal support utils
 inline bool is_orca_open(const std::string& url) { return boost::starts_with(url, "orcaslicer://open"); }
+inline bool is_orcaflashforge_open(const std::string& url) { return boost::starts_with(url, "orcaflashforge://open") || boost::starts_with(url, "orcaflashforgeopen://"); }
 inline bool is_prusaslicer_open(const std::string& url) { return boost::starts_with(url, "prusaslicer://open"); }
 inline bool is_bambustudio_open(const std::string& url) { return boost::starts_with(url, "bambustudio://open") || boost::starts_with(url, "bambustudioopen://"); }
 inline bool is_cura_open(const std::string& url) { return boost::starts_with(url, "cura://open"); }
-inline bool is_supported_open_protocol(const std::string& url) { return is_orca_open(url) || is_prusaslicer_open(url) || is_bambustudio_open(url) || is_cura_open(url); }
-inline bool is_printables_link(const std::string& url) {
+inline bool is_supported_open_protocol(const std::string& url) { return is_orca_open(url) || is_orcaflashforge_open(url) || is_prusaslicer_open(url) || is_bambustudio_open(url) || is_cura_open(url); }
+inline bool is_printables_link(const std::string& url)
+{
     const std::regex url_regex("(http|https)://printables.com", std::regex_constants::icase);
     return std::regex_match(url, url_regex);
 }
