7586a78d4d83d309b83932bbcde82802d5a510d7
linnaiyuan <lny@flashforge.com>
2026-04-30 09:40:34 +0800

    WipeTowerWriter不再将设备类型设置为BBLPrinter，解决擦嘴塔注释输出错误造成的gcode预览异常问题

--- src/libslic3r/GCode/WipeTower.cpp
+++ src/libslic3r/GCode/WipeTower.cpp
@@ -532,7 +532,7 @@ public:
     {
             // ORCA: This class is only used by BBL printers, so set the parameter appropriately.
             // This fixes an issue where the wipe tower was using BBL tags resulting in statistics for purging in the purge tower not being displayed.
-            GCodeProcessor::s_IsBBLPrinter = true;
+            GCodeProcessor::s_IsBBLPrinter = false;
             // adds tag for analyzer:
             std::ostringstream str;
             str << ";" << GCodeProcessor::reserved_tag(GCodeProcessor::ETags::Height) << std::to_string(m_layer_height) << "\n"; // don't rely on GCodeAnalyzer knowing the layer height - it knows nothing at priming
