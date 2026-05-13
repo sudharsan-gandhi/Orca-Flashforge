241fb477afce93aa1e5a116f14a9ef1872037ff7
gengtangrui <tangrui.gen@flashforge.com>
2026-05-13 14:04:48 +0800

    修复切片时材料类型错乱的问题

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -16169,9 +16169,16 @@ void Plater::on_filament_change(size_t filament_idx)
     if (filament_idx >= filament_presets.size())
         return;
     Slic3r::Preset* filament = wxGetApp().preset_bundle->filaments.find_preset(filament_presets[filament_idx]);
-    if (filament == nullptr)
+    Slic3r::Preset* old_filament = &wxGetApp().preset_bundle->filaments.get_selected_preset();
+    if (filament == nullptr || old_filament == nullptr)
         return;
     std::string filament_type = filament->config.option<ConfigOptionStrings>("filament_type")->values[0];
+    std::string old_filament_type = old_filament->config.option<ConfigOptionStrings>("filament_type")->values[0];
+    if (filament_type != old_filament_type) {
+        wxGetApp().preset_bundle->filaments.select_preset_by_name(filament->name, false);
+    }
+    //auto new_filament = wxGetApp().preset_bundle->filaments.get_selected_preset();
+    //new_filament.config;
 }
 
 // BBS.
