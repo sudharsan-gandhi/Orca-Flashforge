34a6f241ce2dd3cc6068f0f810779ca339752ca6
dingjiwei <jiwei.ding@flashforge.com>
2026-05-23 20:45:00 +0800

    修正非法事件构造错误

--- src/slic3r/GUI/FFSelectMachine.cpp
+++ src/slic3r/GUI/FFSelectMachine.cpp
@@ -1179,7 +1179,12 @@ void SelectMachinePopup::Popup(wxWindow *WXUNUSED(focus))
         }
     }
 
-    wxPostEvent(this, wxTimerEvent());
+    CallAfter([this] {
+        wxGetApp().reset_to_active();
+        wxCommandEvent user_event(EVT_UPDATE_USER_MACHINE_LIST);
+        user_event.SetEventObject(this);
+        wxPostEvent(this, user_event);
+    });
     ShowDevList(true);
     FFPopupWindow::Popup();
 }
